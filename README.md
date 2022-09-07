0bc6245fc2fa528cdc7d6a6a256979d71ffee317
Brooke Finister <brookeafinister@gmail.com>
2022-09-07 16:45:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-madlib
+ A Dave Matthews Band lyric inspired Madlib
---
+++ madlib.py
@@ -0,0 +1,81 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+print()
+print("Welcome to my DMB inspired Madlib!")
+print()
+noun1 = input("Choose a noun: ")
+noun2 = input("Choose another noun: ")
+noun3 = input("Choose yet another noun: ")
+verb1 = input("Choose an -ing verb: ")
+adjective = input("Ok, now an adjective: ")
+noun4 = input("Oh geez, another noun: ") 
+noun5 = input("Last one I swear, choose a noun: ")
+noun6 = input("Sike, I lied, last one, for real. Choose a noun: ")
+
+print()
+
+madlib = (
+    
+    f"I say my hell is the {noun1} "
+"I'm stuck inside, "
+f"can't see the {noun2}. "
+f"And my heaven is a nice {noun3} "
+f"in the {noun4}, "
+f"got central {verb1} "
+f"and I'm {adjective}! "
+"Yeah, yeah, yeah... "
+f"Can't see the {noun5}. "
+"Keep it locked up inside, "
+"don't talk about it. "
+f"T-t-talk about the {noun6}! "
+)
+print(      )
+print(madlib)
+print(      )
+
+
+
+
+
+
+
+
+# --------------------------------------------------
+# Partial solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = input("Name:")
+# color = input("color:")
+# num = input("Number:")
+
+# print(f"{name} wore {color} shoes while they counted to {num}")
